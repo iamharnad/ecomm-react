bed7c46cd93b8e6e557033710dcc3a790d09b46e
iamharnad <haranathpriyan@gmail.com>
2022-04-07 13:42:22 +0530

    build
    build the project

--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,6 @@
-// here we define all the application level states and define actions to make the changes to the state
-
 export const initialState = {
   basket: [],
 };
-
-//Selector
 
 export const getBasketTotal = (basket) => {
   return basket?.reduce((amount, item) => item.price + amount, 0);
